14635d1669f87784b20693802fac2b22879118ca
i-takabe <38421033+i-takabe@users.noreply.github.com>
2019-05-20 00:47:28 +0900

    Create unitest2.cpp

---
+++ UnitTest/unitest2.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "anagram.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(Anagram::is_anagram("ABc", "ABC"), true);
+		}
+
+	};
+}
